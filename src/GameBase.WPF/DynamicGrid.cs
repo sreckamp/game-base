9fb69725883667f86c8437de3dba5aec207a711e
Steve Reckamp <sreckamp@gmail.com>
2020-06-09 01:22:46 -0500

    Fix Dynamic Grid

--- src/GameBase.WPF/DynamicGrid.cs
+++ src/GameBase.WPF/DynamicGrid.cs
@@ -13,6 +13,7 @@ namespace GameBase.WPF
         {
             OverCell += (sender, args) => { };
             Background = new SolidColorBrush(Colors.Transparent);
+            Background.Freeze();
         }
 
         #region OverCell
@@ -30,39 +31,48 @@ namespace GameBase.WPF
 
         protected override void OnMouseLeave(MouseEventArgs e)
         {
-            CellEntered(int.MinValue, int.MinValue);
+            if (MonitorMouse)
+            {
+                Debug.WriteLine("DynamicGrid.OnMouseLeave");
+                CellEntered(int.MinValue, int.MinValue);
+            }
+            else
+            {
+                e.Handled = false;
+            }
         }
 
         protected override void OnMouseMove(MouseEventArgs e)
         {
             base.OnMouseMove(e);
-            var p = e.GetPosition(this);
-            if (p.X >= 0 && p.Y >= 0)
+            if (MonitorMouse)
             {
+                var p = e.GetPosition(this);
+                if (p.X < 0 || p.Y < 0) return;
                 int row = 0, column = 0;
                 int x = 0, y = 0;
                 foreach (var cd in ColumnDefinitions)
                 {
-                    x += (int)cd.ActualWidth;
-                    if (p.X < x)
-                    {
-                        break;
-                    }
+                    x += (int) cd.ActualWidth;
+                    if (p.X < x) break;
                     column++;
                 }
+
                 foreach (var rd in RowDefinitions)
                 {
-                    y += (int)rd.ActualHeight;
-                    if (p.Y < y)
-                    {
-                        break;
-                    }
+                    y += (int) rd.ActualHeight;
+                    if (p.Y < y) break;
                     row++;
                 }
+
                 if (row < RowDefinitions.Count && column < ColumnDefinitions.Count)
                 {
                     CellEntered(column, row);
                 }
+            }
+            else
+            {
+                e.Handled = false;
             }
         }
 
@@ -77,38 +87,93 @@ namespace GameBase.WPF
 
         #endregion
 
+        #region LeftClick
+
+        public static readonly RoutedEvent LeftClickEvent = EventManager.RegisterRoutedEvent(
+            nameof(LeftClickEvent), RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(GameGrid));
+
+        public event RoutedEventHandler LeftClick
+        {
+            add => AddHandler(LeftClickEvent, value);
+            remove => RemoveHandler(LeftClickEvent, value);
+        }
+
+        protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonUp(e);
+            RaiseEvent(new RoutedEventArgs(LeftClickEvent, this));
+        }
+
+        #endregion
+
+        #region RightClick
+
+        public static readonly RoutedEvent RightClickEvent = EventManager.RegisterRoutedEvent(
+            nameof(RightClickEvent), RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(GameGrid));
+
+        public event RoutedEventHandler RightClick
+        {
+            add => AddHandler(RightClickEvent, value);
+            remove => RemoveHandler(RightClickEvent, value);
+        }
+
+        protected override void OnMouseRightButtonUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseRightButtonUp(e);
+            RaiseEvent(new RoutedEventArgs(RightClickEvent, this));
+        }
+
+        #endregion
+
+        #region MonitorMouse
+
+        public static readonly DependencyProperty MonitorMouseProperty =
+            DependencyProperty.Register(nameof(MonitorMouse), typeof(bool), typeof(DynamicGrid));
+
+        public bool MonitorMouse
+        {
+            get => (bool)GetValue(MonitorMouseProperty);
+            set => SetValue(MonitorMouseProperty, value);
+        }
+
+        #endregion
+
         #region Columns
 
         public static readonly DependencyProperty ColumnsProperty =
-        DependencyProperty.RegisterAttached(nameof(Columns), typeof(int), typeof(DynamicGrid),
-            new PropertyMetadata(0, ColumnsChanged));
+        DependencyProperty.Register(nameof(Columns), typeof(int), typeof(DynamicGrid),
+            new FrameworkPropertyMetadata(0,
+                FrameworkPropertyMetadataOptions.AffectsMeasure 
+                | FrameworkPropertyMetadataOptions.AffectsMeasure
+                | FrameworkPropertyMetadataOptions.AffectsRender, ColumnsChanged));
 
-        public static void ColumnsChanged(DependencyObject obj,
+        private static void ColumnsChanged(DependencyObject obj,
             DependencyPropertyChangedEventArgs e)
         {
             if (obj is DynamicGrid g)
             {
-                g.Columns = (int)e.NewValue;
+                g.ColumnsChanged((int)e.NewValue);
             }
+        }
+
+        private void ColumnsChanged(int value)
+        {
+            Debug.WriteLine($"DynamicGrid.Columns ColumnDefinition.Count(Before): {ColumnDefinitions.Count}");
+            while (ColumnDefinitions.Count > value)
+            {
+                ColumnDefinitions.RemoveAt(0);
+            }
+            while (ColumnDefinitions.Count < value)
+            {
+                ColumnDefinitions.Add(new ColumnDefinition { Width = m_columnWidth });
+            }
+            Debug.WriteLine($"DynamicGrid.Columns ColumnDefinition.Count (After): {ColumnDefinitions.Count}");
         }
 
         public int Columns
         {
-            get => (int)GetValue(ColumnsProperty);
-            set
-            {
-                Debug.WriteLine($"Columns:{ColumnDefinitions.Count}->{value}");
-                while (ColumnDefinitions.Count > value)
-                {
-                    ColumnDefinitions.RemoveAt(0);
-                }
-                while (ColumnDefinitions.Count < value)
-                {
-                    ColumnDefinitions.Add(new ColumnDefinition { Width = m_columnWidth });
-                }
-                Debug.WriteLine($"After Count = {ColumnDefinitions.Count}");
-                SetValue(ColumnsProperty, value);
-            }
+            get => (int) GetValue(ColumnsProperty);
+            set => SetValue(ColumnsProperty, value);
         }
 
         #endregion
@@ -117,66 +182,72 @@ namespace GameBase.WPF
         private GridLength m_columnWidth = new GridLength(1, GridUnitType.Star);
 
         public static readonly DependencyProperty ColumnWidthProperty =
-        DependencyProperty.RegisterAttached(
+        DependencyProperty.Register(
             nameof(ColumnWidth), typeof(int), typeof(DynamicGrid),
             new PropertyMetadata(-1, ColumnWidthChanged));
 
-        public static void ColumnWidthChanged(DependencyObject obj,
+        private static void ColumnWidthChanged(DependencyObject obj,
             DependencyPropertyChangedEventArgs e)
         {
             if (obj is DynamicGrid g)
             {
-                g.ColumnWidth = (int)e.NewValue;
+                g.ColumnsWidthChanged((int)e.NewValue);
+            }
+        }
+
+        private void ColumnsWidthChanged(int value)
+        {
+            m_columnWidth = value < 0 ? new GridLength(1, GridUnitType.Star) : new GridLength(value);
+            foreach (var cd in ColumnDefinitions)
+            {
+                cd.Width = m_columnWidth;
             }
         }
 
         public int ColumnWidth
         {
-            get => (int)GetValue(ColumnWidthProperty);
-            set
-            {
-                m_columnWidth = value < 0 ? new GridLength(1, GridUnitType.Star) : new GridLength(value);
-                foreach (var cd in ColumnDefinitions)
-                {
-                    cd.Width = m_columnWidth;
-                }
-                SetValue(ColumnWidthProperty, value);
-            }
+            get => (int) GetValue(ColumnWidthProperty);
+            set => SetValue(ColumnWidthProperty, value);
         }
+
         #endregion
 
         #region Rows
 
         public static readonly DependencyProperty RowsProperty =
-            DependencyProperty.RegisterAttached(nameof(Rows), typeof(int), typeof(DynamicGrid),
-                new PropertyMetadata(0, RowsChanged));
+            DependencyProperty.Register(nameof(Rows), typeof(int), typeof(DynamicGrid),
+                new FrameworkPropertyMetadata (0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure 
+                    | FrameworkPropertyMetadataOptions.AffectsMeasure
+                    | FrameworkPropertyMetadataOptions.AffectsRender, RowsChanged));
 
-        public static void RowsChanged(DependencyObject obj,
+        private static void RowsChanged(DependencyObject obj,
             DependencyPropertyChangedEventArgs e)
         {
             if (obj is DynamicGrid g)
             {
-                g.Rows = (int)e.NewValue;
+                g.RowsChanged((int)e.NewValue);
             }
+        }
+
+        private void RowsChanged(int value)
+        {
+            Debug.WriteLine($"DynamicGrid.Rows RowDefinitions.Count(Before): {RowDefinitions.Count}");
+            while (RowDefinitions.Count > value)
+            {
+                RowDefinitions.RemoveAt(0);
+            }
+            while (RowDefinitions.Count < value)
+            {
+                RowDefinitions.Add(new RowDefinition { Height = m_rowHeight });
+            }
+            Debug.WriteLine($"DynamicGrid.Rows RowDefinitions.Count (After): {RowDefinitions.Count}");
         }
 
         public int Rows
         {
             get => (int)GetValue(RowsProperty);
-            set
-            {
-                Debug.WriteLine($"Rows:{RowDefinitions.Count}->{value}");
-                while (RowDefinitions.Count > value)
-                {
-                    RowDefinitions.RemoveAt(0);
-                }
-                while (RowDefinitions.Count < value)
-                {
-                    RowDefinitions.Add(new RowDefinition { Height = m_rowHeight });
-                }
-                Debug.WriteLine($"After Count = {RowDefinitions.Count}");
-                SetValue(RowsProperty, value);
-            }
+            set => SetValue(RowsProperty, value);
         }
 
         #endregion
@@ -185,31 +256,31 @@ namespace GameBase.WPF
         private GridLength m_rowHeight = new GridLength(1, GridUnitType.Star);
 
         public static readonly DependencyProperty RowHeightProperty =
-            DependencyProperty.RegisterAttached(nameof(RowHeight), typeof(int), typeof(DynamicGrid),
+            DependencyProperty.Register(nameof(RowHeight), typeof(int), typeof(DynamicGrid),
                 new PropertyMetadata(-1, RowHeightChanged));
 
-        public static void RowHeightChanged(DependencyObject obj,
+        private static void RowHeightChanged(DependencyObject obj,
             DependencyPropertyChangedEventArgs e)
         {
             if (obj is DynamicGrid g)
             {
-                g.RowHeight = (int)e.NewValue;
+                g.RowHeightChanged((int) e.NewValue);
+            }
+        }
+
+        private void RowHeightChanged(int value)
+        {
+            m_rowHeight = value < 0 ? new GridLength(1, GridUnitType.Star) : new GridLength(value);
+            foreach (var rd in RowDefinitions)
+            {
+                rd.Height = m_rowHeight;
             }
         }
 
         public int RowHeight
         {
             get => (int)GetValue(RowHeightProperty);
-
-            set
-            {
-                m_rowHeight = value < 0 ? new GridLength(1, GridUnitType.Star) : new GridLength(value);
-                foreach (var rd in RowDefinitions)
-                {
-                    rd.Height = m_rowHeight;
-                }
-                SetValue(RowHeightProperty, value);
-            }
+            set => SetValue(RowHeightProperty, value);
         }
 
         #endregion
