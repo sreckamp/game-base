33a539196687a8402374d4af3b3bff7700d7c935
Steve Reckamp <sreckamp@gmail.com>
2020-06-14 16:48:06 -0500

    nuspec

---
+++ src/GameBase.Model.Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace GameBase.Model.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
